3da8af0f9dbc1a5f4a409645f6c8c3e6c11b4dbb
Josh Newman <josh.newman4390@gmail.com>
2023-10-28 11:39:24 +1100

    Set up default route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { NextPageContext } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const { from, to } = context.query;
+
+  console.log({ from, to });
+
+  return {
+    props: {},
+  };
+};
 
 export default function Home() {
+  const router = useRouter();
+
+  // Redirect to default currencies if there are no query params
+  useEffect(() => {
+    if (!router.query.from || !router.query.to) {
+      router.replace({
+        pathname: "/",
+        query: {
+          from: "AUD",
+          to: "JPY",
+        },
+      });
+    }
+  }, [router]);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -20,7 +48,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -52,7 +80,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -69,7 +97,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -86,7 +114,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -103,7 +131,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -114,5 +142,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
